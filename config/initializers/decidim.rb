7e809147f45e0d6ae8b729a903a5e071e48a7579
will94290 <will94290@yahoo.fr>
2017-11-02 12:54:38 +0100

    mails + langue

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,13 +1,14 @@
+# coding: utf-8
 # frozen_string_literal: true
 
 Decidim.configure do |config|
   config.application_name = "My Application Name"
-  config.mailer_sender = "change-me@domain.org"
-  config.authorization_handlers = ["ExampleAuthorizationHandler"]
+  config.mailer_sender = "mako@osp.cat"
+  config.authorization_handlers = ["Decidim::DummyAuthorizationHandler"]
 
   # Change these lines to set your preferred locales
   config.default_locale = :en
-  config.available_locales = [:en, :ca, :es]
+  config.available_locales = [:en, :ca, :es, :fr]
 
   # Geocoder configuration
   # config.geocoder = {
