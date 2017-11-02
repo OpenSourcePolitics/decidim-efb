569a0ccf0f6c454fad33471998ff883f069a95e0
Ludivine Coipel <coipel.ludivine@gmail.com>
2017-11-02 18:43:29 +0100

    configure settings polis (ucv, ucw, show-vis)

--- decidim-polis/app/controllers/decidim/polis/polis_controller.rb
+++ decidim-polis/app/controllers/decidim/polis/polis_controller.rb
@@ -5,7 +5,10 @@ module Decidim
     # Exposes the polis resource so users can view them
     class PolisController < Decidim::Polis::ApplicationController
       def show
-        @feature_id = params[:feature_id]
+        @feature = Feature.find(params[:feature_id])
+        @ucw = @feature.settings.user_can_write?
+        @ucv = @feature.settings.user_can_vote?
+        @show_vis = @feature.settings.visualization?
       end
     end
   end
