8eec9b3746abaed28d480f2310d54ed3d8ecdf12
will94290 <will94290@yahoo.fr>
2017-11-09 16:40:15 +0100

    polis_id + file.json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  $filepath = '/home/mako/decidim-efb/.file.json'
+
+  $country = File.read($filepath)
+
+  $tab_word = JSON.parse($country)
 end
