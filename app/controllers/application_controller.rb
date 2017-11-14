ee193b642c3f318c18539407abaddf54c744fd80
moustachu <git@moustachu.net>
2017-11-14 16:32:35 +0100

    Fix mapping : Decidim org name <> Pol.is site ID

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  $filepath = '/home/mako/decidim-efb/.file.json'
-
-  $country = File.read($filepath)
-
-  $tab_word = JSON.parse($country)
 end
