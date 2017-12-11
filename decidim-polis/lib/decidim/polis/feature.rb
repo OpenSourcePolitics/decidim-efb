072c3f4be75424c8604af8427faf29e8f3426cc8
virgile-dev <virgile.deville@gmail.com>
2017-12-11 16:43:48 +0100

    create repo

--- decidim-polis/lib/decidim/polis/feature.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_dependency "decidim/features/namer"
-
-Decidim.register_feature(:polis) do |feature|
-  feature.engine = Decidim::Polis::Engine
-  feature.icon = "decidim/polis/icon.svg"
-end
